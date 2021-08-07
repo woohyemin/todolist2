5087480888b72b84d41e92393d10dd1d1c26c1f3
hyemin <woo28870826@gmail.com>
2021-08-08 00:50:33 +0900

    complete

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled, {css} from 'styled-components';
 import { MdAdd } from 'react-icons/md';
+import { useTodoDispatch, useTodoNextId } from '../TodoContext';
 
 const CircleButton = styled.button`
     background: #43608E;
@@ -52,7 +53,8 @@ const InsertFormPositioner = styled.div`
     position: absolute;
 `;
 
-const InsertForm = styled.div`
+// div 말고 form으로 하면 onSubmit 가능
+const InsertForm = styled.form`
     background: #f8f9fa;
     padding: 32px;
     padding-bottom: 72px;
@@ -74,15 +76,42 @@ const Input = styled.input`
 
 function TodoCreate() {
     const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('');
+    const dispatch = useTodoDispatch();
+    const nextId = useTodoNextId();
+
     const onToggle = () => setOpen(!open);
+    const onChange = e => setValue(e.target.value);
+    const onSubmit = e => {
+        e.preventDefault();
+        dispatch({
+            type: 'CREATE',
+            todo: {
+                id: nextId.current,
+                text: value,
+                done: false,
+            }
+            
+        });
+        setValue('');
+        setOpen(false);
+        nextId.current += 1;
+    };
+
+
 
     
     return (
         <>
             {open && (
                 <InsertFormPositioner>
-                    <InsertForm>
-                        <Input placeholder="할 일을 입력 후, Enter를 누르세요" autoFocus/>
+                    <InsertForm onSubmit={onSubmit}>
+                        <Input
+                        placeholder="할 일을 입력 후, Enter를 누르세요"
+                        autoFocus
+                        onChange={onChange}
+                        value={value}
+                        />
                     </InsertForm>
                 </InsertFormPositioner>
             )}
@@ -93,4 +122,5 @@ function TodoCreate() {
     );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
+//React.memo를 넣어주면 컴포넌트 최적화 가능(해당되지 않는 부분들은 리렌더링되지 않음)
