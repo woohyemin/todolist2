33894250b8828a61a16e957e7b58d5f1a13835a6
hyemin <woo28870826@gmail.com>
2021-08-11 23:09:08 +0900

    add description 3

--- src/TodoContext.js
+++ src/TodoContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useReducer, useContext, useRef } from 'react';
 
-const initialTodos = [
+const initialTodos = [ // 할 일 초기 세팅
     {
         id: 1,
         text: '프로젝트 생성하기',
@@ -31,14 +31,14 @@ const initialTodos = [
 
 function todoReducer(state, action) {
     switch (action.type) {
-        case 'CREATE':
-            return state.concat(action.todo);
-        case 'TOGGLE':
+        case 'CREATE': // 생성
+            return state.concat(action.todo); // state에 action.todo 합쳐서 리턴
+        case 'TOGGLE': // 토글 (체크 아이콘)
             return state.map(
                 todo => todo.id === action.id ? { ...todo, done: !todo.done } : todo
-            );
-        case 'REMOVE':
-            return state.filter(todo => todo.id !== action.id);
+            ); // todo.id가 action.id와 같을 경우 기존 todo(할 일)에 done을 반대로 바꿔주고, 같지 않으면 기존 todo(할 일) 그대로 리턴
+        case 'REMOVE': // 삭제
+            return state.filter(todo => todo.id !== action.id); // action.id가 아닌 (= 삭제되지 않은) todo(할 일만) 리턴
 
         default:
             throw new Error(`Unhandled action type: ${action.type}`);
@@ -53,7 +53,7 @@ const TodoNextIdContext = createContext();
 
 export function TodoProvider({ children }) {
     const [state, dispatch] = useReducer(todoReducer, initialTodos);
-    const nextId = useRef(5);
+    const nextId = useRef(5); // 다음 id 값에 5 넣음
     return (
         <TodoStateContext.Provider value={state}>
             <TodoDispatchContext.Provider value={dispatch}>
